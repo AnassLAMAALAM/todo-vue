083c2c9f39b4e2ca87132768d6e19ce97845bca2
anasslam21 <anasslam69@gmail.com>
2019-09-24 20:59:50 +0100

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="todo-item">
+            <div class="todo-item-left">
+            <input type="checkbox" v-model="completed"
+            @change="doneEdit">
+            
+                <div v-if="!editing" @dblclick="editTodo" class="todo-item-label" 
+                :class="{ completed : completed }">{{ todo.title }}</div>
+                <input v-else class="todo-item-edit" type="text" v-model="title" 
+                @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" >
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">
+                &times;
+            </div>
+    </div>
+</template>
+<script>
+export default{
+    name:'todo-item',
+    props:{
+        todo:{
+            type:Object,
+            required :true,
+        },
+        index:{
+            type:Number,
+            required:true,
+        },
+        chekAll:{
+            type:Boolean,
+            required:true,
+        }
+    },
+    data(){
+        return{
+            'id' : this.todo.id,
+            'title' : this.todo.title,
+            'completed' : this.todo.completed,
+            'editing' : this.todo.editing,
+            'beforeEditCache':''
+        }
+    },
+    watch:{
+        chekAll(){
+            this.completed = this.chekAll ? true : this.todo.completed
+        }
+    },
+    methods:{
+        removeTodo(index){
+            eventBus.$emit('removedTodo',index);
+        },
+        editTodo(){
+            this.beforeEditCache = this.title;
+            this.editing = true
+        },
+        doneEdit() {
+        if (this.title.trim() == '') {
+            this.title = this.beforeEditCache
+        }
+        this.editing = false
+            eventBus.$emit('finishedEdit',{
+            'index':this.index,
+            'todo' :{
+                'id':this.id,
+                'title':this.title,
+                'completed':this.completed,
+                'editing':this.editing,
+            }
+        });
+        },
+        cancelEdit() {
+        this.title = this.beforeEditCache
+        this.editing = false
+        }
+    }
+}
+</script>
+<style>
+
+</style>
